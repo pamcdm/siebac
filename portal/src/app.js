8328e87715a4e46fc11365c0165f8b78ae45f36f
Marcos Vinicius <k@bymarkone.com>
2015-02-28 18:15:48 -0300

    Adds Express mapping to public resources and welcome page

--- portal/src/app.js
+++ portal/src/app.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
   res.send('Hello World!')
-})
+});
+
+app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function () {
 
