e0ecd48829db30a2ed176016bc8828eedb1c2951
Marcos Vinicius <k@bymarkone.com>
2015-03-01 01:20:09 -0300

    Adds naive but fully working gulp build in three phases: src, build, dist

---
+++ portal/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var lib = require('bower-files')({
+  json: './src/site/bower.json',
+  dir: './src/site/bower_components',
+  overrides: {
+    react: {
+      main: ['react.js', 'JSXTransformer.js'],
+      dependencies: {}
+    }
+  }
+});
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/site/style/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/site/style'));
+});
+
+gulp.task('bower', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(gulp.dest('./build/site/lib/'))
+});
+
+gulp.task('distLibs', function() {
+  return gulp.src('./build/site/lib/*.js')
+    .pipe(gulp.dest('./dist/public/lib'));
+});
+
+gulp.task('distSources', function() {
+  return gulp.src('./src/site/*.*')
+    .pipe(gulp.dest('./dist/public'));
+});
+
+gulp.task('distStyles', ['sass'], function() {
+  return gulp.src(['./build/site/style/*.css'])
+    .pipe(gulp.dest('./dist/public/style'));
+});
+
+gulp.task('default', ['distLibs', 'distSources', 'distStyles'], function() {
+
+});
+
