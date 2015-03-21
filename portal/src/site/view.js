2ba21c0a5462d38e47b59384c1ed6f041b0b59e6
Marcos Vinicius <k@bymarkone.com>
2015-03-21 17:08:20 -0300

    Start working in portal styles

--- portal/src/site/view.js
+++ portal/src/site/view.js
@@ -1,4 +1,23 @@
+var CarSide = React.createClass({
+  render: function() {
+    return <section class="car-chooser">
+      <h3>Escolha o seu carro</h3>
+    </section>  
+  }
+});
+
+var AlternativeSide = React.createClass({
+  render: function() {
+    return <section class="alternative-chooser">
+      <h3>Agora, se você não tivesse um carro, você usaria?</h3>
+    </section>  
+  }
+});
+
 React.render(
-  <h1>Hello, world!</h1>,
+  <div>   
+    <CarSide/>
+    <AlternativeSide/>
+  </div>,
   document.getElementById('main')
 );
